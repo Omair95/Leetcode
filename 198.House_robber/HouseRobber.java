7234b458640187015e2f93545171dfdccafe6ba7
Omair Iqbal <omair95@protonmail.com>
2022-01-15 15:39:43 +0000

    Leetcode 198

--- 198.House_robber/HouseRobber.java
+++ 198.House_robber/HouseRobber.java
@@ -1,12 +1,20 @@
+import java.util.Arrays;
 
 public class HouseRobber {
 
     public static int rob(int[] nums) {
-        return 0;
+        int dp[] = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 1; i < nums.length; ++i) {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+
+        return dp[nums.length];
     }
 
     public static void main (String [] args) {
-        System.out.println(rob(new int[] { 1, 2, 3, 1 }));
-        System.out.println(rob(new int[] { 2, 7, 9, 3, 1 }));
+        System.out.println(rob(new int[] { 50, 1, 1, 50 }));
     }
 }
