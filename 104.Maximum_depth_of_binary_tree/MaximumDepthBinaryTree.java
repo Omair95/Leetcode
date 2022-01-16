d72a40985698ee5ab94ec53a053870ce0658cb0b
Omair Iqbal <omair95@protonmail.com>
2022-01-16 20:06:18 +0000

    Leetcode 104

---
+++ 104.Maximum_depth_of_binary_tree/MaximumDepthBinaryTree.java
@@ -0,0 +1,25 @@
+
+public class MaximumDepthBinaryTree {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        else return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
---
+++ 49.Group_anagrams/GroupAnagrams.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        return null;
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
