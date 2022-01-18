4723ac459b0967a02edab98ddc2ce1a8c8c01335
Omair Iqbal <omair95@protonmail.com>
2022-01-18 20:21:47 +0000

    Leetcode 206

---
+++ 230.Kth_smallest_element_in_BST/KthSmallestElementInBST.java
@@ -0,0 +1,27 @@
+import java.util.Set;
+import java.util.SortedSet;
+
+public class KthSmallestElementInBST {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+
+
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
