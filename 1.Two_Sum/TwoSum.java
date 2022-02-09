3da2e18b93b9931dc10ecb7bd7358c96ae761e28
Omair Iqbal <omair95@protonmail.com>
2022-02-09 20:28:38 +0000

    Leetcode 1 with 3 diff solutions

--- 1.Two_Sum/TwoSum.java
+++ 1.Two_Sum/TwoSum.java
@@ -1,83 +1,17 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
 public class TwoSum {
 
+    // Brute force
     public static int[] twoSumBruteForce(int[] nums, int target) {
-
-        int[] result = {};
+        int[] res = new int[] {};
 
         for (int i = 0; i < nums.length; ++i) {
             for (int j = i + 1; j < nums.length; ++j) {
                 if ((nums[i] + nums[j]) == target) {
-                    result = new int[] {i, j};
-                    return result;
-                }
-
-            }
-        }
-
-        return result;
-    }
-
-    public static int[] twoSumHashMap(int[] nums, int target) {
-        int[] result = new int[2];
-        Map<Integer, Integer> temp = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (temp.containsKey(target - nums[i])) {
-                result[1] = i;
-                result[0] = temp.get(target - nums[i]);
-                return result;
-            }
-            temp.put(nums[i], i);
-        }
-
-        return result;
-    }
-
-    public static int[] twoSumPointers(int[] nums, int target) {
-        int[] nums2 = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(nums2);
-
-        int a = 0, b = 0;
-        int l = 0, r = nums2.length-1;
-
-        while(l < r){
-            int sum = nums2[l] + nums2[r];
-            if(sum < target)
-                l++;
-            else if(sum > target)
-                r--;
-            else{
-                a = nums2[l];
-                b = nums2[r];
-                break;
-            }
-        }
-
-
-        int[] res = new int[2];
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == a) {
-                res[0] = i;
-                break;
-            }
-        }
-
-        if(a != b) {
-            for(int i = 0; i < nums.length; i++) {
-                if(nums[i] == b) {
-                    res[1] = i;
-                    break;
-                }
-            }
-        } else {
-            for(int i = 0; i < nums.length; i++) {
-                if(nums[i] == b && i != res[0]) {
-                    res[1] = i;
-                    break;
+                    res = new int[] { i, j };
+                    return res;
                 }
             }
         }
@@ -85,9 +19,54 @@ public class TwoSum {
         return res;
     }
 
+    // Using auxiliary hashmap to store visited numbers
+    public static int[] twoSumHashMap(int[] nums, int target) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        int[] res = new int[] {};
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (m.containsKey(target - nums[i])) {
+                res = new int[] { i, m.get(target - nums[i]) };
+                return res;
+            }
+
+            m.put(nums[i], i);
+        }
+
+        return res;
+    }
+
+    // Using two pointers pattern
+    public static int[] twoSumTwoPointers(int[] nums, int target) {
+        int[] copy = nums.clone();
+        Arrays.sort(nums);
+
+        int l = 0, r = nums.length - 1, i = 0, j = 0;
+
+        while (l <= r) {
+            int sum = nums[l] + nums[r];
+            if (sum == target) {
+                i = nums[l];
+                j = nums[r];
+                break;
+            }
+            else if (sum < target) ++l;
+            else --r;
+        }
+
+        int res1 = 0, res2 = 0;
+
+        for (int k = 0; k < copy.length; ++k) {
+            if (copy[k] == i && res1 == 0) res1 = k;
+            if (copy[k] == j && k != res1) res2 = k;
+        }
+
+        return new int[] { res1, res2 };
+    }
+
     public static void main (String [] args) {
         System.out.println(Arrays.toString(twoSumBruteForce(new int[] { 2, 3, 4, 5, 9, 11 }, 11)));
         System.out.println(Arrays.toString(twoSumHashMap(new int[] { 2, 3, 4, 5, 9, 11 }, 11)));
-        System.out.println(Arrays.toString(twoSumPointers(new int[] { 2, 3, 4, 5, 9, 11 }, 11)));
+        System.out.println(Arrays.toString(twoSumTwoPointers(new int[] { 2, 5, 5, 11 }, 10)));
     }
 }
